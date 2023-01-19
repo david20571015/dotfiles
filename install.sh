4d1f8a7fbf4586f287fed831b58a36e4da7ef104
David Chiu <david20571015@gmail.com>
2023-01-19 23:49:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-rm ~/.zshrc
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
 
